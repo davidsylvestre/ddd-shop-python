6db9698e3bf1b4d1d7fc8c07470509a8a5b173fe
Frederico Cabral <frederico.cabral@corp.globo.com>
2015-09-15 14:32:15 -0300

    Basic ProductRepository

--- catalog/models.py
+++ catalog/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 
 
-class Product(models.Model):
+class ProductData(models.Model):
+
+    class Meta:
+        db_table = 'catalog_product'
+
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=6, decimal_places=2)
--- catalog/views.py
+++ catalog/views.py
@@ -1,19 +1,42 @@
 from django.views.generic import View
 from django.shortcuts import render
 
-from catalog.models import Product
+from catalog.models import ProductData
+
+from mapper.object_mapper import ObjectMapper
+
+
+class Product(object):
+
+    def __init__(self, name, description, price):
+        self.name = name
+        self.description = description
+        self.price = price
+
+
+class ProductRepository(object):
+
+    def __init__(self):
+        self.mapper = ObjectMapper()
+        self.mapper.create_map(Product, ProductData)
+
+    def save(self, product):
+        product_data = self.mapper.map(product, ProductData)
+        product_data.save()
+
+    def all(self):
+        return ProductData.objects.all()
 
 
 class CatalogView(View):
 
     def get(self, request):
 
-        # product = Product()
-        # product.name = 'Teste'
-        # product.description = 'Teste Description'
-        # product.price = 10
-        # product.save()
+        product = Product('Vai', 'Descricao do Livro', 10)
 
-        products = Product.objects.all()
+        repository = ProductRepository()
+        repository.save(product)
+
+        products = repository.all()
 
         return render(request, 'catalog/catalog_home.html', {'products': products})
